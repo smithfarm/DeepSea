smithfarm/DeepSea

commit 9f450a2ebe4caf56412128c9465ef4097ee51339

Moved salt-api validation from stage 0 to stage 1
Signed-off-by: Ricardo Dias <rdias@suse.com>

srv/salt/ceph/stage/prep/master/default-no-update-no-reboot.sls
@@ -1,8 +1,3 @@
-salt-api:
-  salt.state:
-    - tgt: {{ salt['pillar.get']('master_minion') }}
-    - sls: ceph.salt-api
-
 {% if salt['saltutil.runner']('validate.setup') == False %}
 
 validate failed:
@@ -13,15 +8,10 @@ validate failed:
 
 {% endif %}
 
-{% if salt['saltutil.runner']('validate.saltapi') == False %}
-
-salt-api failed:
+salt-api:
   salt.state:
-    - name: just.exit
     - tgt: {{ salt['pillar.get']('master_minion') }}
-    - failhard: True
-
-{% endif %}
+    - sls: ceph.salt-api
 
 sync master:
   salt.state:

srv/salt/ceph/stage/prep/master/default-no-update-reboot.sls
@@ -1,8 +1,3 @@
-salt-api:
-  salt.state:
-    - tgt: {{ salt['pillar.get']('master_minion') }}
-    - sls: ceph.salt-api
-
 {% if salt['saltutil.runner']('validate.setup') == False %}
 
 validate failed:
@@ -13,15 +8,10 @@ validate failed:
 
 {% endif %}
 
-{% if salt['saltutil.runner']('validate.saltapi') == False %}
-
-salt-api failed:
+salt-api:
   salt.state:
-    - name: just.exit
     - tgt: {{ salt['pillar.get']('master_minion') }}
-    - failhard: True
-
-{% endif %}
+    - sls: ceph.salt-api
 
 sync master:
   salt.state:

srv/salt/ceph/stage/prep/master/default-update-no-reboot.sls
@@ -1,8 +1,3 @@
-salt-api:
-  salt.state:
-    - tgt: {{ salt['pillar.get']('master_minion') }}
-    - sls: ceph.salt-api
-
 {% if salt['saltutil.runner']('validate.setup') == False %}
 
 validate failed:
@@ -13,15 +8,10 @@ validate failed:
 
 {% endif %}
 
-{% if salt['saltutil.runner']('validate.saltapi') == False %}
-
-salt-api failed:
+salt-api:
   salt.state:
-    - name: just.exit
     - tgt: {{ salt['pillar.get']('master_minion') }}
-    - failhard: True
-
-{% endif %}
+    - sls: ceph.salt-api
 
 sync master:
   salt.state:

srv/salt/ceph/stage/prep/master/default.sls
@@ -1,8 +1,3 @@
-salt-api:
-  salt.state:
-    - tgt: {{ salt['pillar.get']('master_minion') }}
-    - sls: ceph.salt-api
-
 {% if salt['saltutil.runner']('validate.setup') == False %}
 
 validate failed:
@@ -13,15 +8,10 @@ validate failed:
 
 {% endif %}
 
-{% if salt['saltutil.runner']('validate.saltapi') == False %}
-
-salt-api failed:
+salt-api:
   salt.state:
-    - name: just.exit
     - tgt: {{ salt['pillar.get']('master_minion') }}
-    - failhard: True
-
-{% endif %}
+    - sls: ceph.salt-api
 
 sync master:
   salt.state:

srv/salt/ceph/stage/discovery/default.sls
@@ -1,3 +1,13 @@
+{% if salt['saltutil.runner']('validate.saltapi') == False %}
+
+salt-api failed:
+  salt.state:
+    - name: just.exit
+    - tgt: {{ salt['pillar.get']('master_minion') }}
+    - failhard: True
+
+{% endif %}
+
 {% if salt['saltutil.runner']('validate.prep') == False %}
 
 validate failed:

commit ce7c6cc5afa02b10cf62c9aea7f1be08fb9c6d0a

Sync modules before deploying salt-api
This appears to be a regression introduced by
9f450a2ebe4caf56412128c9465ef4097ee51339 which was merged via
https://github.com/SUSE/DeepSea/pull/718 on October 6, 2017.

If salt-api is deployed before syncing modules, Stage 0 fails with:

Rendering SLS 'base:ceph.salt-api.configure' failed: Jinja variable
'salt.utils.templates.AliasedLoader object' has no attribute 'deepsea.user'

Fixes: https://github.com/SUSE/DeepSea/issues/914
Signed-off-by: Denis Kondratenko <denis.kondratenko@suse.com>
Signed-off-by: Nathan Cutler <ncutler@suse.com>

srv/salt/ceph/stage/prep/master/default-no-update-no-reboot.sls
@@ -8,15 +8,15 @@ validate failed:
 
 {% endif %}
 
-salt-api:
-  salt.state:
-    - tgt: {{ salt['pillar.get']('master_minion') }}
-    - sls: ceph.salt-api
-
 sync master:
   salt.state:
     - tgt: {{ salt['pillar.get']('master_minion') }}
     - sls: ceph.sync
+
+salt-api:
+  salt.state:
+    - tgt: {{ salt['pillar.get']('master_minion') }}
+    - sls: ceph.salt-api
 
 {% set notice = salt['saltutil.runner']('advise.salt_run') %}
 

srv/salt/ceph/stage/prep/master/default-no-update-reboot.sls
@@ -8,15 +8,15 @@ validate failed:
 
 {% endif %}
 
-salt-api:
-  salt.state:
-    - tgt: {{ salt['pillar.get']('master_minion') }}
-    - sls: ceph.salt-api
-
 sync master:
   salt.state:
     - tgt: {{ salt['pillar.get']('master_minion') }}
     - sls: ceph.sync
+
+salt-api:
+  salt.state:
+    - tgt: {{ salt['pillar.get']('master_minion') }}
+    - sls: ceph.salt-api
 
 {% set notice = salt['saltutil.runner']('advise.salt_run') %}
 

srv/salt/ceph/stage/prep/master/default-update-no-reboot.sls
@@ -8,15 +8,15 @@ validate failed:
 
 {% endif %}
 
-salt-api:
-  salt.state:
-    - tgt: {{ salt['pillar.get']('master_minion') }}
-    - sls: ceph.salt-api
-
 sync master:
   salt.state:
     - tgt: {{ salt['pillar.get']('master_minion') }}
     - sls: ceph.sync
+
+salt-api:
+  salt.state:
+    - tgt: {{ salt['pillar.get']('master_minion') }}
+    - sls: ceph.salt-api
 
 {% set notice = salt['saltutil.runner']('advise.salt_run') %}
 

srv/salt/ceph/stage/prep/master/default.sls
@@ -8,15 +8,15 @@ validate failed:
 
 {% endif %}
 
-salt-api:
-  salt.state:
-    - tgt: {{ salt['pillar.get']('master_minion') }}
-    - sls: ceph.salt-api
-
 sync master:
   salt.state:
     - tgt: {{ salt['pillar.get']('master_minion') }}
     - sls: ceph.sync
+
+salt-api:
+  salt.state:
+    - tgt: {{ salt['pillar.get']('master_minion') }}
+    - sls: ceph.salt-api
 
 {% set notice = salt['saltutil.runner']('advise.salt_run') %}
 